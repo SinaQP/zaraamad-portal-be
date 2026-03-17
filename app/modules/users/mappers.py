ac40981745e48e7b4421fc10f4c139c07824c06c
Sina <qasempoursina@gmail.com>
2026-03-17 15:58:24 +0330

    i

--- app/modules/users/mappers.py
+++ app/modules/users/mappers.py
@@ -13,7 +13,7 @@ class UserMapper:
             is_active=user.is_active,
             created_at=user.created_at,
             updated_at=user.updated_at,
-            password=user.password
+            password=''
         )
 
 
